413e0eaa341e0a01d31c492c3627b3c1875783b9
Andas Puranda <Puranda.external@infineon.com>
2024-12-26 09:36:51 +0700

    Add project files.

---
+++ FinanceApi/Controllers/StockController.cs
@@ -0,0 +1,35 @@
+﻿using FinanceApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+        public StockController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocs = _context.Stocks.ToList();
+            return Ok(stocs);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
---
+++ FinanceApi/Data/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FinanceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
