69c848e4154e130fcf55e4bba89cb6bd088b2375
EatMyLunch <113190595+EatMyLunch@users.noreply.github.com>
2025-01-01 11:15:37 +0700

    number 21

--- FinanceApi/Data/ApiDbContext.cs
+++ FinanceApi/Data/ApiDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using FinanceApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceApi.Data
 {
-    public class ApiDbContext : DbContext
+    public class ApiDbContext : IdentityDbContext<AppUser>
     {
         public ApiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
         public DbSet<Stock> Stocks { get; set; }
