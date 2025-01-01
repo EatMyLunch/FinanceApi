e609a8171fd1326b532c0809939d614699ee2a9b
EatMyLunch <113190595+EatMyLunch@users.noreply.github.com>
2025-01-01 11:45:42 +0700

    number 22

--- FinanceApi/Data/ApiDbContext.cs
+++ FinanceApi/Data/ApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceApi.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +10,26 @@ namespace FinanceApi.Data
         public ApiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
